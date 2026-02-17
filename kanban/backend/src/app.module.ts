d711c3c734a721900d0505b4bbae0b1bc1efc954
Lucas Genova <glucasemanuel@gmail.com>
2026-02-17 13:34:05 -0300

    feat: add boards, columns and cards modules

--- kanban/backend/src/app.module.ts
+++ kanban/backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
+import { ColumnsModule } from './columns/columns.module';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule, ColumnsModule, CardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
