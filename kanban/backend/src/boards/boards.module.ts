58477a080b735d36808a8bfa24edd7571fe40ccb
Lucas Genova <glucasemanuel@gmail.com>
2026-02-21 09:49:02 -0300

    fix: backend modules

--- kanban/backend/src/boards/boards.module.ts
+++ kanban/backend/src/boards/boards.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { ColumnsService } from 'src/columns/columns.service';
+import { ColumnsController } from 'src/columns/columns.controller';
+import { CardsModule } from 'src/cards/cards.module';
 
 @Module({
-  controllers: [BoardsController],
-  providers: [BoardsService],
+  imports: [CardsModule], 
+  controllers: [BoardsController, ColumnsController],
+  providers: [BoardsService, ColumnsService],
 })
 export class BoardsModule {}
--- kanban/backend/src/columns/columns.module.ts
+++ kanban/backend/src/columns/columns.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ColumnsController } from './columns.controller';
 import { ColumnsService } from './columns.service';
+import { CardsService } from 'src/cards/cards.service';
+import { CardsController } from 'src/cards/cards.controller';
 
 @Module({
-  controllers: [ColumnsController],
-  providers: [ColumnsService],
+  controllers: [ColumnsController, CardsController],
+  providers: [ColumnsService, CardsService],
 })
 export class ColumnsModule {}
